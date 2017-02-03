5c90e2f0e557c689299bd4b34efc7a5c783aa94d
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2017-02-03 05:42:44 -0500

    modify auth0 settings

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import Feed from './components/Feed.vue'
 
 export const lock = new Auth0Lock(
   'EIhU9jVy0uIxD43R7Bkp2EfgTMNJN4B3',
-  'ayanga.auth0.com'
+  'ayanga.auth0.com',
+  {
+    allowedConnections: ["twitter"],
+    rememberLastLogin: true,
+    socialButtonStyle: "big",
+    theme: {"primaryColor":"#3A99D8"},
+    languageDictionary: {"title":"Twivo"}
+  }
 )
 
 new Vue({
